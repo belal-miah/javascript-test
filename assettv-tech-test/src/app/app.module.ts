09e6de7a4ac953d00d9f09198ce45069846acb80
Belal Miah <belal.miah@just-eat.com>
2018-11-19 23:02:20 +0000

    Created Header and Body components

--- assettv-tech-test/src/app/app.module.ts
+++ assettv-tech-test/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { VideoGridComponent } from './video-grid/video-grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    VideoGridComponent
   ],
   imports: [
     BrowserModule,
